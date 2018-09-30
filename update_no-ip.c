6af9c4503bd68f83e2bd9ba97f12027805faff60
marumori <kota_taku@outlook.jp>
2018-09-30 11:28:48 +0900

    bug fixed:update_no-ip.c

--- update_no-ip.c
+++ update_no-ip.c
@@ -3,14 +3,16 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #define ONE_HOUR 3600
 #define TWO_HOUR 7200
 #define INFINITE_LOOP 1
-#define PID_FILE "/root/pract_directory/04_no-ip/update_no-ip.pid"
+#define BUFF_LEN 256
 
 int update_curltimer();
 int create_timerlimit(unsigned int min,unsigned int max,unsigned int *response);
+int retry_curl();
 
 int
 update_curltimer(){
@@ -19,7 +21,6 @@ update_curltimer(){
 	unsigned int minSecond = ONE_HOUR;
 	unsigned int maxSecond = TWO_HOUR;
 	unsigned int limitSecond = 0;
-	char *curlURL = "curl -s ";
 
 	create_timerlimit(minSecond,maxSecond,&limitSecond);
 	endTime = time(NULL) + limitSecond;
@@ -32,14 +33,34 @@ update_curltimer(){
 		sleep(5);
 	}
 	
-	system(curlURL);
+	system("curl -s YOUR-USERNAME:password@dynupdate.no-ip.com/nic/update?hostname=domain name > result file");
+	retry_curl();
+	
 	return 0;
 }
 
 
 int
 create_timerlimit(unsigned int min,unsigned int max,unsigned int *response){
-	*response = min +(unsigned int)(rand()*(max-min+1)/(1+RAND_MAX));
+	*response = min +(unsigned int)(rand()*(max-min+1)/(1+(unsigned int)RAND_MAX));
+	return 0;
+}
+
+int
+retry_curl(){
+	FILE *fp_read;
+	char buffer[BUFF_LEN];	
+
+	fp_read = fopen(RETRY_CHECK_FILE,"r");
+	if(NULL == fp_read){
+		return 1;
+	}
+	fgets(buffer,BUFF_LEN,fp_read);
+	while(0 == strncmp(buffer,"badauth",7)){
+		system("curl -s YOUR-USERNAME:password@dynupdate.no-ip.com/nic/update?hostname=domain name > result file");
+	}
+
+	fclose(fp_read);
 	return 0;
 }
 
@@ -49,7 +70,6 @@ main(int argc,char *argv[]){
 	if(0 == daemon(0,0)){
 		while(INFINITE_LOOP){
 			update_curltimer();
-		sleep(5);
 		}
 	}else{
 		printf("%s:Error",argv[0]);
