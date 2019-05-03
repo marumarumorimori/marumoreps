16f4cb749e30f18649a0d5ace59a6f6db85c810d
marumori <kota_taku@outlook.jp>
2019-05-03 17:25:07 +0900

    Fixed class.cc

--- C++/class.cc
+++ C++/class.cc
@@ -4,6 +4,10 @@
 #include <uuid/uuid.h>      //Need -luuid when compiling.
 #include <stdio.h>
 
+#define UUID_LENGHT        32
+#define OK                 0
+#define NG                 1
+
 class Account{
     private:
         std::string name_;
@@ -21,26 +25,28 @@ class Account{
         const std::string get_id() const noexcept{ return id_; }
 };
 
+std::string create_uuid();
+int create_account(std::map<std::string, Account>& account_map);
+int serch_account(std::map<std::string, Account>& account_map);
+
 std::string create_uuid(){
     uuid_t uuid;
-    char string[32];
+    char string[UUID_LENGHT];
 
     uuid_generate(uuid);
     uuid_unparse_upper(uuid, string);
 
-    std::string st_string(string, 32);
+    std::string st_string(string, UUID_LENGHT);
 
     printf("Your key is \"%s\"\n", st_string.c_str());
 
     return st_string;
 }
 
-int main(void){
+int create_account(std::map<std::string, Account>& account_map){
     std::string name = "";
     std::string remarks = "";
     std::string id = "";
-    std::string key = "";
-    std::map<std::string, Account> account_map;
     int loop = 0;
 
     for(loop=0; 2>loop; ++loop){
@@ -60,10 +66,18 @@ int main(void){
         account_map.insert(std::make_pair(account.get_id() ,account));
 
         //Standard output data.
+        printf("----------------------------------------------");
         printf("Name\t:%s\n", account.get_name().c_str());
         printf("Remarks\t:%s\n", account.get_remarks().c_str());
         printf("\n");
     }
+    printf("----------------------------------------------");
+
+    return OK;
+}
+
+int serch_account(std::map<std::string, Account>& account_map){
+    std::string key = "";
 
     //Receive key of value that user wants to acquire.
     printf("Please enter your key :");
@@ -78,7 +92,15 @@ int main(void){
     }else{
         printf("Key Error.\n");
     }
-    
-    return 0;
+    return OK;
+}
+
+int main(void){
+    std::map<std::string, Account> account_map;
+
+    create_account(account_map);
+    serch_account(account_map);
+
+    return OK;
 }
 
