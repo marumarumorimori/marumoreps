c4f6a20eef8539e70e6b76e11200c5579c5a0864
marumori <kota_taku@outlook.jp>
2019-06-30 13:55:17 +0900

    fixed

--- C++/02_test.cc
+++ C++/02_test.cc
@@ -7,6 +7,24 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctime>
+
+
+typedef struct connection_info{
+    std::time_t connection_time;
+    
+}connection_info_t;
+
+class SecureLogReader{
+    public:
+
+    protected:
+
+    private:
+        std::string         ip_address_;
+        
+
+};
 
 std::vector<std::string> split(const std::string &str, char separator){
     std::vector<std::string> vect;
@@ -20,50 +38,31 @@ std::vector<std::string> split(const std::string &str, char separator){
     return vect;
 }
 
-bool search_ssh_failure(std::string &line_element, bool failed_msg_flag){
-    std::string failure_msg("Failed");
-
-    if(failure_msg == line_element){
+bool search_for_word(std::string serch_word, std::string &line_element, bool now_flag){
+    if(serch_word == line_element){
         return true;
-    }else if(true == failed_msg_flag){
+    }else if(true == now_flag){
         return true;
     }
 
     return false;
 }
 
-bool search_user_name_failure(std::string &line_element, bool failed_user_name){
-    std::string failure_user("invalid");
-
-    if(failure_user == line_element){
-        return true;
-    }else if(true == failed_user_name){
-        return true;
-    }
-
-    return false;
-}
-
-bool str_to_int(const char *str_num, int &ret_num){
+bool str_to_int(const char *str_num, int &int_number){
     char *endptr = NULL;
-    long l_number = strtol(str_num, &endptr, 10);
- 
+    long long_number = strtol(str_num, &endptr, 10);
+
     if(0 != strncmp("\0", endptr, 1)){
         std::cout << "Conversion error. [" << *endptr << "]" << std::endl;
-        std::cout << "In :" << &str_num <<std::endl;
-        std::cout << "Err:" << &endptr <<std::endl;
         return false;
     }
     
-    if(INT_MAX < l_number ||  INT_MIN > l_number){
+    if(INT_MAX < long_number ||  INT_MIN > long_number){
         std::cout << "Buffer over flow." << std::endl;
         return false;
     }
 
-    std::cout << "In :" << str_num <<std::endl;
-    std::cout << "Err:" << endptr <<std::endl;
-
-    ret_num = (int)l_number;
+    int_number = (int)long_number;
     return true;
 }
 
@@ -74,6 +73,11 @@ int main(int argc, char **argv){
     int limit = 0;
     int count = 0;
     bool ret = false;
+
+    if((NULL == argv[1]) || (NULL == argv[2])){
+        std::cout << "Argument error." <<std::endl;
+        return false;
+    }
 
     ret = str_to_int(argv[2], limit);
     if(false == ret){
@@ -89,18 +93,18 @@ int main(int argc, char **argv){
     while(getline(ifs, file_line)){
         int loop = 0;
         bool failed_msg_flag = false;
-        bool failed_user_name = false;
+        bool failed_user_name_flag = false;
 
         list = split(file_line, ' ');
         for(auto list_element: list){
             ++loop;
-            failed_msg_flag = search_ssh_failure(list_element, failed_msg_flag);
-            failed_user_name = search_user_name_failure(list_element, failed_user_name);
+            failed_msg_flag = search_for_word("Failed", list_element, failed_msg_flag);
+            failed_user_name_flag = search_for_word("invalid", list_element, failed_user_name_flag);
             if(failed_msg_flag){
-                if(!failed_user_name && 11 == loop){
+                if(!failed_user_name_flag && 11 == loop){
                     log_map_data["IP Address"] = list_element;
                     std::cout << "IP Address : " << log_map_data["IP Address"] <<  std::endl;
-                }else if(failed_user_name && 11 == loop){
+                }else if(failed_user_name_flag && 11 == loop){
                     log_map_data["User Name"] = list_element;
                     std::cout << "User Name : " << log_map_data["User Name"] << std::endl;
                 }
